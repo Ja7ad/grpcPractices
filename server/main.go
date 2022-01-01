c3d29e3b90d341807e6661537c66b97e18924136
Javad <ja7ad@live.com>
2022-01-01 16:10:33 +0330

    Add balance service server side response data to client

--- server/main.go
+++ server/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
-	pb "github.com/Ja7ad/greeting/protos"
+	"github.com/Ja7ad/grpcPractices/pb"
 	"google.golang.org/grpc"
+	"io/ioutil"
 	"log"
 	"net"
+	"os"
+	"time"
 )
 
 const (
@@ -19,28 +22,69 @@ var (
 )
 
 type server struct {
-	pb.UnimplementedGreeterServer
+	pb.UnimplementedBalanceServiceServer
 }
 
-func (s *server) SayHello(ctx context.Context, in *pb.GreetRequest) (*pb.GreetResponse, error) {
-	log.Printf("Received: %v", in.GetName())
-	return &pb.GreetResponse{Message: "Hello " + in.GetName()}, nil
+type accountsBalance []struct {
+	Id      uint32
+	Name    string
+	Balance []struct {
+		Type string
+		Fund float64
+	}
 }
 
 func main() {
-	flag.Parse()
-
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%v", *port))
+	log.Println("starting balance service...")
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
 	if err != nil {
 		log.Fatalln(err)
 	}
 
+	log.Printf("Listen Requests from address %v", lis.Addr())
 	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{})
-
-	log.Printf("server listen at address %v", lis.Addr())
-
-	if errRPC := s.Serve(lis); errRPC != nil {
-		log.Fatalln(errRPC)
+	pb.RegisterBalanceServiceServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalln(err)
 	}
+}
+
+// loadBalanceFromFile load balance accounts from file for testing in server
+func loadBalanceFromFile() (accountsBalance, error) {
+	var balance accountsBalance
+
+	jsonFile, err := os.Open("/home/javad/Work/Project/go/Practices/grpcPractices/server/balance.json")
+	if err != nil {
+		return nil, err
+	}
+
+	byteJson, _ := ioutil.ReadAll(jsonFile)
+
+	if err := json.Unmarshal(byteJson, &balance); err != nil {
+		return nil, err
+	}
+
+	return balance, nil
+}
+
+// GetBalance is implemented for rpc function
+func (s *server) GetBalance(in *pb.BalanceReq, stream pb.BalanceService_GetBalanceServer) error {
+	balance, err := loadBalanceFromFile()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("New request for ID %v", in.GetId())
+
+	for _, b := range balance {
+		if b.Id == in.GetId() {
+			for _, balanceItems := range b.Balance {
+				res := &pb.BalanceResp{Id: b.Id, Name: b.Name, Type: balanceItems.Type, Balance: balanceItems.Fund}
+				stream.Send(res)
+				time.Sleep(1000 * time.Millisecond)
+			}
+		}
+	}
+
+	return nil
 }
